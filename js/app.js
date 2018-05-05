b5c33c13e4ab26b37c517dff31074b21e243b80f
Michael <mkroth65@gmail.com>
2018-05-04 22:57:35 -0500

    Added images, create and shuffle cards, assign images to cards, added click eventhandler.

--- js/app.js
+++ js/app.js
@@ -1,7 +1,27 @@
+/* jshint esversion: 6 */
+
+class Card {
+	constructor(image, matched, index) {
+		this.image = image; // String
+		this.matched = matched; // Boolean
+		this.index = index;
+	}
+}
+
+const images = ['crab.svg', 'dolphin.svg', 'fish.svg', 'lemonade.svg',
+'palm-trees.svg', 'sailboat.svg', 'snorkel.svg', 'sun.svg'];
+//function sCard(symbol: any, matched: any): void
 /*
  * Create a list that holds all of your cards
  */
+let cardList = [];
 
+// Add 8sets of two identical cards
+// Index is used to compare cards for match
+for(let i = 0; i < 8; i++){
+	cardList.push(new Card(images[i], false, i));
+	cardList.push(new Card(images[i], false, i));
+}
 
 /*
  * Display the cards on the page
@@ -12,19 +32,30 @@
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
-    var currentIndex = array.length, temporaryValue, randomIndex;
+	var currentIndex = array.length,
+		temporaryValue, randomIndex;
 
-    while (currentIndex !== 0) {
-        randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex -= 1;
-        temporaryValue = array[currentIndex];
-        array[currentIndex] = array[randomIndex];
-        array[randomIndex] = temporaryValue;
-    }
+	while (currentIndex !== 0) {
+		randomIndex = Math.floor(Math.random() * currentIndex);
+		currentIndex -= 1;
+		temporaryValue = array[currentIndex];
+		array[currentIndex] = array[randomIndex];
+		array[randomIndex] = temporaryValue;
+	}
 
-    return array;
+	return array;
 }
 
+function addImageToCards(){
+	let cardImage = document.querySelectorAll('.card-image');
+	cardImage.forEach(function(img, index){
+		img.setAttribute('src', 'img/' + cardList[index].image);
+	});
+	let cards = document.querySelectorAll('.card');
+	cards.forEach(function (card, index){
+		card.setAttribute('id', index);
+	});
+}
 
 /*
  * set up the event listener for a card. If a card is clicked:
@@ -36,3 +67,19 @@ function shuffle(array) {
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
+
+shuffle(cardList);
+addImageToCards();
+const deck = document.querySelector('.deck');
+//console.log(deck);
+deck.addEventListener('click', function (event){
+	let card = event.target;
+	//console.log(card.nodeName);
+	if(card.nodeName == 'LI'){
+		console.log('clicked a card');
+		console.log(card.getAttribute('id'));
+		let cardID = card.getAttribute('id');
+		console.log(cardList[cardID]);
+	}
+	//event.target.style.visibility = 'hidden';
+});
